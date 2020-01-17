dcbd3d3a4333e85cdf35143fe8af98fcfb1707f2
Owain West <owainwest>
2020-01-17 01:32:15 -0500

    minor text fixes

--- run_classifier_scope.py
+++ run_classifier_scope.py
@@ -264,6 +264,7 @@ def convert_single_example(ex_index, example, label_list, max_seq_length,
         label_id=0,
         is_real_example=False)
 
+  label_map = {}
   for (i, label) in enumerate(label_list):
     label_map[label] = i
 
